e406d774e596a85691391954dcc78c0bfe51a7a8
SharlotteMaxswell <charlotte.maxwell@hotmail.co.uk>
2024-11-15 09:26:24 +0000

    Added regfile content

---
+++ ALU.sv
@@ -0,0 +1,12 @@
+module ALU #(
+    DATA_WIDTH = 32
+) (
+    input logic     ALUop1,
+    input logic     ALUop2,
+    input logic     ALUctrl,
+    output logic    ALUout,
+    output logic    eq
+
+);
+    
+endmodule
