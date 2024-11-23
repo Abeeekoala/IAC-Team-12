a3fe5cd501141c9e7df739b66b952b2ed89e8a19
Shravankumar05 <mshravan005@gmail.com>
2024-11-23 16:31:04 +0000

    CU structured

--- modules/CU.sv
+++ modules/CU.sv
@@ -1,22 +1,4 @@
 module CU (
-)(
-<<<<<<< Updated upstream
-    input logic [2:0]   funct3,
-    input logic [6:0]   opcode,
-    input logic         funct7,
-    input logic         EQ,
-    output logic [1:0]  ImmSrc,
-    output logic        PCsrc,
-    output logic        ALUsrc,
-    output logic [2:0]  ALUctrl,
-    output logic        RegWrite
-);
-
-always_comb begin
-    ImmSrc = 2'b0;
-    PCsrc = 1'b0;
-    ALUsrc = 1'b0;
-=======
     input logic [2:0] funct3,
     input logic [6:0] opcode,
     input logic [6:0] funct7,
@@ -35,7 +17,6 @@ always_comb begin
     PCSrc = 1'b0;
     ResultSrc = 1'b0;
     ALUSrc = 1'b0;
->>>>>>> Stashed changes
     ALUctrl = 3'b000;
     RegWrite = 1'b0;
     MemWrite = 1'b0;
@@ -109,20 +90,12 @@ always_comb begin
         // i-type instructions
         7'b0010011: begin
             RegWrite = 1'b1;
-<<<<<<< Updated upstream
-            ALUsrc = 1'b1;
-            ImmSrc = 2'b0;
-=======
             ALUSrc = 1'b1;
->>>>>>> Stashed changes
 
             case(funct3)
                 // Addi
                 3'b000: begin
                     ALUctrl = 3'b000;
-                end
-                default: begin
-                    // to be implemented
                 end
             endcase
 
@@ -140,12 +113,6 @@ always_comb begin
 
         // b-type instructions
         7'b1100011: begin
-<<<<<<< Updated upstream
-            RegWrite = 1'b0;
-            ALUsrc = 1'b0;
-            ImmSrc = 2'b0;
-=======
->>>>>>> Stashed changes
             case(funct3)
                 //bne
                 3'b001:begin
@@ -153,9 +120,6 @@ always_comb begin
                     ALUctrl = 3'b001;
                 end
             endcase
-        end
-        default:    begin
-            // to be implemented
         end
     
     endcase
