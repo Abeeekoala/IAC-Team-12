ef970687f34cd3ec25ada42bb31c7a7134c64ff3
Shravankumar05 <mshravan005@gmail.com>
2024-12-02 19:53:26 +0000

    top, datapath, PC partial  implementation fixes

--- modules/top.sv
+++ modules/top.sv
@@ -9,10 +9,14 @@ logic [31:0]        ImmOp;
 logic               PCsrc ;
 logic [31:0]        InstrAdd;
 logic [31:0]        instr;
-logic               eq;
-logic [2:0]         ALUctrl;
+logic               Zero;
+logic               Less;
+logic               LessU;
+logic [3:0]         ALUctrl;
 logic               ALUsrc;
-logic [1:0]         ImmSrc;
+logic               MemWrite;
+logic               ResultSrc;
+logic [2:0]         ImmSrc;
 logic               RegWrite;  
 
 PC PC(
@@ -29,14 +33,18 @@ InstrMem InstrMem(
 );
 
 CU CU(
-    .opcode     (instr[6:0]),
     .funct3     (instr[14:12]),
-    .funct7     (instr[30]),
-    .EQ         (eq),
-    .PCsrc      (PCsrc),
-    .ALUctrl    (ALUctrl),
-    .ALUsrc     (ALUsrc),
+    .op         (instr[6:0]),
+    .funct7_5   (instr[30]),
+    .Zero       (Zero),
+    .Less       (Less),
+    .LessU      (LessU),
     .ImmSrc     (ImmSrc),
+    .PCSrc      (PCsrc),
+    .ResultSrc  (ResultSrc),
+    .ALUsrc     (ALUsrc),
+    .MemWrite   (MemWrite),
+    .ALUctrl    (ALUctrl),
     .RegWrite   (RegWrite)
 );
 
@@ -48,10 +56,10 @@ sign_ext sign_ext(
 
 regfile regfile_instance(
     .clk(clk),
-    .A1 (),
-    .A2 (),
-    .A3 (),
-    .WE3 (),
+    .A1 (instr[19:15]),
+    .A2 (instr[24:20]),
+    .A3 (instr[11:7]),
+    .WE3 (RegWrite),
     .WD3 (result),
     .RD1 (ALUop1),
     .RD2 (regOp2),
@@ -69,7 +77,7 @@ datapath datapath(
     .ALUctrl    (ALUctrl),
     .ALUsrc     (ALUsrc),
     .ImmOp      (ImmOp),
-    .eq         (eq),
+    .Zero       (Zero),
     .a0         (a0)
 );
 
