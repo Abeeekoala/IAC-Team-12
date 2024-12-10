2fac2326a419fa1496394db83d8b571071388a57
Abeeekoala <abrahamlin0120@gmail.com>
2024-12-10 20:01:20 +0000

    Minor Fixed on hazardunit_tb.cpp
    Co-Authored-By: ShravanKumar05 <mshravan005@gmail.com>

--- tb/tests/hazardunit_tb.cpp
+++ tb/tests/hazardunit_tb.cpp
@@ -24,67 +24,67 @@ protected:
     }
 };
 
-TEST_F(HazardUnit_Testbench, ForwardA_WB)
+TEST_F(HazardUnitTestbench, ForwardA_WB)
 {
-    top->Rs1E = 29;
-    top->RdM = 29;
+    top->Rs1E = 0b00101;
+    top->RdM = 0b00101;
     top->RegWriteM = 1;
     top->eval();
 
     EXPECT_EQ(top->ForwardA, 2);
 }
 
-TEST_F(HazardUnit_Testbench, ForwardA_MEM)
+TEST_F(HazardUnitTestbench, ForwardA_MEM)
 {
-    top->Rs1E = 29;
-    top->RdW = 29;
-    top->RegWriteW = 1;
+    top->Rs1E = 0b00101;
+    top->RdM = 0b00101;
+    top->RegWriteM = 1;
     top->eval();
 
-    EXPECT_EQ(top->ForwardA, 1);
+    EXPECT_EQ(top->ForwardA, 2);
 }
 
-TEST_F(HazardUnit_Testbench, ForwardB_WB)
+TEST_F(HazardUnitTestbench, ForwardB_WB)
 {
-    top->Rs2E = 29;
-    top->RdW = 29;
-    top->RegWriteW = 1'b1;
+    top->Rs2E = 0b00101;
+    top->RdW = 0b00101;
+    top->RegWriteW = 1;
     top->eval();
 
     EXPECT_EQ(top->ForwardB, 1);
 }
 
-TEST_F(HazardUnit_Testbench, ForwardB_MEM)
+TEST_F(HazardUnitTestbench, ForwardB_MEM)
 {
-    top->Rs2E = 29;
-    top->RdM = 29;
-    top->RegWriteW = 1;
+    top->Rs2E = 0b00101;
+    top->RdM = 0b00101;
+    top->RegWriteM = 1;
     top->eval();
 
     EXPECT_EQ(top->ForwardB, 2);
 }
 
-TEST_F(HazardUnit_Testbench, Stall1)
+TEST_F(HazardUnitTestbench, Stall1)
 {
-    top->Rs1D = 29;
-    top->RdE = 29;
-    top->LoadE = 1;
-    top->eval();
-
-    EXPECT_EQ(top->Flush, 1);
-}
-
-TEST_F(HazardUnit_Testbench, Stall2)
-{
-    top->Rs2D = 29;
-    top->RdE = 29;
+    top->Rs1D = 0b00101;
+    top->RdE = 0b00101;
     top->LoadE = 1;
     top->eval();
 
     EXPECT_EQ(top->Stall, 1);
 }
 
-TEST_F(HazardUnit_Testbench, Flush1)
+TEST_F(HazardUnitTestbench, Stall2)
+{
+    top->Rs2D = 0b00101;
+    top->RdE = 0b00101;
+    top->LoadE = 1;
+    top->eval();
+
+    EXPECT_EQ(top->Stall, 1);
+}
+
+TEST_F(HazardUnitTestbench, Flush1)
 {
     top->rst = 1;
     top->eval();
@@ -92,7 +92,7 @@ TEST_F(HazardUnit_Testbench, Flush1)
     EXPECT_EQ(top->Flush, 1);
 }
 
-TEST_F(HazardUnit_Testbench, Flush2)
+TEST_F(HazardUnitTestbench, Flush2)
 {
     top->PCSrc = 1;
     top->eval();
