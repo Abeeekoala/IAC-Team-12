5f3eeced64d26f846d4e9bed8806a995dc63cca0
Abeeekoala <abrahamlin0120@gmail.com>
2024-12-11 01:28:03 +0000

    Plotting PDF complete.

--- tb/plots/plot_tb.cpp
+++ tb/plots/plot_tb.cpp
@@ -3,6 +3,8 @@
 #include "verilated_vcd_c.h"
 #include "../../vbuddy.cpp"
 
+
+
 int main(int argc, char **argv, char **env){
     int i;
     int clk;
@@ -26,7 +28,7 @@ int main(int argc, char **argv, char **env){
     top->clk = 1;
     top->rst = 0;
     int plot = 0;
-    bool to_plot = false; 
+    bool to_load = false; 
     int last_a0 = -1;
     //run simulation for many clock cycles
     for (i=0; i<1000000; i++){
@@ -35,7 +37,7 @@ int main(int argc, char **argv, char **env){
             top->clk = !top->clk;   // unit is in ps!!!
             top->eval ();
         }
-        if (plot == 0 && top->a0 == 210)
+        if (plot == 0 && top->a0 == 205)
         {
             plot = 1;
         }
@@ -43,20 +45,16 @@ int main(int argc, char **argv, char **env){
             break;
         }
         if (plot > 0){
-            if (top->a0 == 210 && to_plot){
-                to_plot = false;
-                vbdPlot(int(last_a0), 0, 255);
-                plot++;
+            if (top->a0 == 205){
+                to_load = true;
             }
-            else if (top->a0 == 210){
-                to_plot = true;
-                last_a0 = top->a0;
+            else if (top->a0 != 205 && to_load){
+                to_load = false;
+                plot++;
+                vbdPlot(int(top->a0), 0, 255);
+
             }
         }
-        
-        
-
-        vbdCycle(i+1);
             // either simulation finished, or 'q' is pressed
         if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
             exit(0);                // ... exit if finish OR 'q' pressed
